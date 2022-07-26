629e81dc28461ac1c99d0089cf54a4a22aeece19
Jorge Vitor <jorgevitorp9@gmail.com>
2022-07-26 12:55:45 -0300

    ✨ feat(menu): options to filter by region

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,17 @@
 import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
+import { MenuComponent } from "../../components/menu/menu.component";
 
 export class HomePage extends HTMLElement {
     constructor() {
         super()
     }
 
-    private readonly declarations = [SearchBarComponent]
+    private readonly declarations = [SearchBarComponent, MenuComponent]
     connectedCallback() {
         this.innerHTML = `
         <div class="navigation">
             <rca-search-bar></rca-search-bar>
+            <rca-menu></rca-menu>
         </div>
         `;
         
