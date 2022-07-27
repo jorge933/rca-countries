577686121b1a470b9e94ae00c4d19a1a31ab138b
Jorge Vitor <jorgevitorp9@gmail.com>
2022-07-27 14:29:56 -0300

    ✨ feat(home): filter countries by name

--- src/components/search-bar/search-bar.component.ts
+++ src/components/search-bar/search-bar.component.ts
@@ -1,14 +1,44 @@
+import { SearchCountriesService } from "../../service/search-countries.service";
+import { UtilsService } from "../../service/Utils.service";
 import searchBarTemplate from "./search-bar.component.html";
 import searchBarStyles from "./search-bar.component.scss";
 
 export class SearchBarComponent extends HTMLElement {
+    private searchCountriesService = new SearchCountriesService();
     constructor() {
         super();
+        this.filterByName = this.filterByName.bind(this)
     }
 
     connectedCallback() {
         const styles = searchBarStyles;
         this.innerHTML = searchBarTemplate;
+
+        const $input = this.querySelector('input');
+
+        $input.addEventListener('input', event => {
+            this.filterByName(event);
+        })
+    }
+
+    private async filterByName(event: Event) {
+        const filter = document.querySelector('.selected span').innerHTML;
+        const target = event.target as HTMLTextAreaElement
+        const value = target.value.toLowerCase();
+
+        let filteredByName = await this.searchCountriesService.filterByName(value);
+        
+        if (filter !== "Filter By Region") {
+            const countriesFiltered = await this.searchCountriesService.filterByRegion(filter, filteredByName);
+            console.log(countriesFiltered);
+            
+            filteredByName = countriesFiltered;
+            console.log(filteredByName);
+            
+        }
+        
+
+        UtilsService.renderCountries(filteredByName);
     }
 }
 
