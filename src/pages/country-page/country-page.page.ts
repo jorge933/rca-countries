c5f8dcc51785f19bffcc0c49a9431df9f84426cf
Jorge Vitor <jorgevitorp9@gmail.com>
2022-07-27 12:59:53 -0300

    ✨ feat(country-page): adding informations dynamically

--- src/pages/country-page/country-page.page.ts
+++ src/pages/country-page/country-page.page.ts
@@ -1,15 +1,39 @@
+import { RestCountriesApiService } from "../../service/rest-countries-api.service";
+import { UtilsService } from "../../service/Utils.service";
 import countryTemplate from "./country-page.page.html";
 import countryStyles from "./country-page.page.scss";
+import { Currency, NativeName } from "../../models/country.model";
 
 export class CountryPage extends HTMLElement {
+    private restCountriesApiService = new RestCountriesApiService();
     constructor() {
         super();
     }
 
-    connectedCallback() {
+    async connectedCallback() {
         const styles = countryStyles;
-        this.innerHTML = countryTemplate;
+
+        const countryCode = this.getAttribute('country-code');
+        
+        const country = await this.restCountriesApiService.getCountry(countryCode);
+        console.log(country)
+        const {flags, name, population, region, subregion, capital, flag, currencies, languages, borders} = country[0];
+        const maskedPopulation = population.toLocaleString();
+        const allLangs = Object.values(languages).join(', ');
+        const currency : Currency[] = Object.values(currencies);
+        const nativeName : NativeName[] = Object.values(name.nativeName);
+        
+        this.removeAttribute('country-code');
+        this.innerHTML = UtilsService.bindModelToView(countryTemplate, {flag: flags.svg, topLevelDomain: flag, name: name.common, maskedPopulation, nativeName: nativeName[0].common, region, subregion, capital, currency: currency[0].name, allLangs});
+        this.borderCountries(borders)
     }
+
+    borderCountries (borders: string[]) {
+        const $borders = document.querySelector('.border-countries');
+        borders.forEach(border => $borders.innerHTML += `<div>${border}</div>`)
+    }
+
+
 }
 
 customElements.define('rca-country-page', CountryPage)
--- src/service/rest-countries-api.service.ts
+++ src/service/rest-countries-api.service.ts
@@ -1,7 +1,8 @@
 import { StorageService } from "./storage.service";
+import { Country } from "../models/country.model";
 
 export class RestCountriesApiService {
-    async getAllCountries() {
+    async getAllCountries() : Promise<Country> {
         const countriesInStorage = StorageService.getItem('countries');
         
         if (!countriesInStorage) {
@@ -13,7 +14,22 @@ export class RestCountriesApiService {
             return countriesArray;
         }
 
-        const countriesArray = JSON.parse(countriesInStorage);
+        const countriesArray = await JSON.parse(countriesInStorage);
         return countriesArray;
+    }
+
+    async getCountry(countryCode) : Promise<Country> {
+        const countryInStorage = StorageService.getItem(countryCode);
+
+        if (!countryInStorage) {
+            const country = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+            const countryArray = await country.json();
+            const countryStringify = JSON.stringify(countryArray);
+            StorageService.setItem(countryCode, countryStringify);
+            return countryArray;
+        }
+
+        const countryArray = await JSON.parse(countryInStorage);
+        return countryArray;
     }
 }
