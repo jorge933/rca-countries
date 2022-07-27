275098633288a5ab1d17e90f8f151ae96a175b76
Jorge Vitor <jorgevitorp9@gmail.com>
2022-07-27 14:25:11 -0300

    ✨ feat(search-countries.service.ts): filterByName

---
+++ src/service/search-countries.service.ts
@@ -0,0 +1,18 @@
+import { RestCountriesApiService } from "./rest-countries-api.service";
+import { Country } from "../models/country.model";
+
+export class SearchCountriesService {
+    private restCountriesApiService = new RestCountriesApiService();
+
+    async filterByName(name: string, countries : Country[] = undefined) {
+        if(!countries) countries = await this.restCountriesApiService.getAllCountries();
+        
+        return countries.filter(country => {
+            const countryNameInLowerCase = country.name.common.toLowerCase();
+            const searchNameInLowerCase = name;
+
+            if(countryNameInLowerCase.startsWith(searchNameInLowerCase)) return country;
+        });
+    }
+
+}
