3293480a10a1de7808d8462b048baa16808841ae
Jorge Vitor <jorgevitorp9@gmail.com>
2022-09-30 14:34:04 -0300

    ✨ feat(index.ts): dark mode

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 const $main = document.querySelector("#root");
+const $changeTheme = document.querySelector(".modes");
+const $body = document.body;
 import { ROUTES } from "./constants/routes";
-
+import { StorageService } from "./service/storage.service";
 class SinglePageApplication {
+  currentMode: string | null;
   constructor() {
+    this.currentMode = StorageService.getItem("mode") || "light";
     this.renderPage = this.renderPage.bind(this);
     this.renderPage();
     this.hashListener();
     this.windowLoadListener();
+    this.initTheme();
   }
 
   getTargetRoute(hash: string) {
@@ -33,6 +38,38 @@ class SinglePageApplication {
       this.renderPage();
       this.hashListener();
     });
+  }
+  changeTheme() {
+    if (this.currentMode === undefined) {
+      this.currentMode = StorageService.getItem("mode");
+    }
+
+    $body.classList.toggle("dark");
+    const newTheme = this.currentMode === "light" ? "dark" : "light";
+    const classIcon = newTheme === "light" ? "sun" : "moon";
+
+    console.log(this.currentMode);
+
+    $changeTheme!.innerHTML = `
+    <i class="fa-solid fa-${classIcon}"></i>
+    <span>${newTheme} mode</span>
+    `;
+    this.currentMode = newTheme;
+    StorageService.setItem("mode", newTheme);
+  }
+
+  initTheme() {
+    if (this.currentMode === "dark") {
+      $body.classList.add("dark");
+    }
+    const classIcon = this.currentMode === "light" ? "sun" : "moon";
+
+    $changeTheme!.innerHTML = `
+    <i class="fa-solid fa-${classIcon}"></i>
+    <span>${this.currentMode} mode</span>
+    `;
+
+    StorageService.setItem("mode", this.currentMode!);
   }
 }
 
